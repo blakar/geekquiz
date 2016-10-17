606ee6b4ca06050f8459a07fb7fc676f50d29ee4
blakar <blakar@users.noreply.github.com>
2016-10-17 11:58:42 -0700

    Implement POST api/Trivia

--- GeekQuiz/Controllers/TriviaController.cs
+++ GeekQuiz/Controllers/TriviaController.cs
@@ -6,9 +6,42 @@
     using System.Web.Http;
     using System.Linq;
     using System.Threading;
+    using System.Web.Http.Description;
+
     public class TriviaController : ApiController
     {
         private TriviaContext db = new TriviaContext();
+
+        // GET api/Trivia
+        [ResponseType(typeof(TriviaQuestion))]
+        public async Task<IHttpActionResult> Get()
+        {
+            var userId = User.Identity.Name;
+
+            TriviaQuestion nextQuestion = await this.NextQuestionAsync(userId);
+
+            if (nextQuestion == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(nextQuestion);
+        }
+
+        // POST api/Trivia
+        [ResponseType(typeof(TriviaAnswer))]
+        public async Task<IHttpActionResult> Post(TriviaAnswer answer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.BadRequest(this.ModelState);
+            }
+
+            answer.UserId = User.Identity.Name;
+
+            var isCorrect = await this.StoreAsync(answer);
+            return this.Ok<bool>(isCorrect);
+        }
 
         protected override void Dispose(bool disposing)
         {
@@ -34,6 +67,17 @@
 
             var nextQuestionId = (lastQuestionId % questionsCount) + 1;
             return await this.db.TriviaQuestions.FindAsync(CancellationToken.None, nextQuestionId);
+        }
+
+        private async Task<bool> StoreAsync(TriviaAnswer answer)
+        {
+            this.db.TriviaAnswers.Add(answer);
+
+            await this.db.SaveChangesAsync();
+            var selectedOption = await this.db.TriviaOptions
+                .FirstOrDefaultAsync(o => o.Id == answer.OptionId && o.QuestionId == answer.QuestionId);
+
+            return selectedOption.IsCorrect;
         }
     }
 }
