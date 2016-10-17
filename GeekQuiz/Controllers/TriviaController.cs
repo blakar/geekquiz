d4a5e80d5e8363d80d948c3118963da1827d1bd0
blakar <blakar@users.noreply.github.com>
2016-10-17 00:20:06 -0700

    Implement Dispose() of Trivia Controller

--- GeekQuiz/Controllers/TriviaController.cs
+++ GeekQuiz/Controllers/TriviaController.cs
@@ -1,13 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-
-namespace GeekQuiz.Controllers
+﻿namespace GeekQuiz.Controllers
 {
+    using GeekQuiz.Models;
+    using System.Web.Http;
+
     public class TriviaController : ApiController
     {
+        private TriviaContext db = new TriviaContext();
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
     }
 }
