a8c6cdce07b230ba3e90ef9604912f05bb412f9c
blakar <blakar@users.noreply.github.com>
2016-10-17 11:50:08 -0700

    Add NextQuestionAsync helper method

--- GeekQuiz/Controllers/TriviaController.cs
+++ GeekQuiz/Controllers/TriviaController.cs
@@ -1,8 +1,11 @@
 ﻿namespace GeekQuiz.Controllers
 {
     using GeekQuiz.Models;
+    using System.Data.Entity;
+    using System.Threading.Tasks;
     using System.Web.Http;
-
+    using System.Linq;
+    using System.Threading;
     public class TriviaController : ApiController
     {
         private TriviaContext db = new TriviaContext();
@@ -15,6 +18,22 @@
             }
 
             base.Dispose(disposing);
+        }
+
+        private async Task<TriviaQuestion> NextQuestionAsync(string userId)
+        {
+            var lastQuestionId = await this.db.TriviaAnswers
+                .Where(a => a.UserId == userId)
+                .GroupBy(a => a.QuestionId)
+                .Select(g => new { QuestionId = g.Key, Count = g.Count() })
+                .OrderByDescending(q => new { q.Count, QuestionId = q.QuestionId })
+                .Select(q => q.QuestionId)
+                .FirstOrDefaultAsync();
+
+            var questionsCount = await this.db.TriviaQuestions.CountAsync();
+
+            var nextQuestionId = (lastQuestionId % questionsCount) + 1;
+            return await this.db.TriviaQuestions.FindAsync(CancellationToken.None, nextQuestionId);
         }
     }
 }
